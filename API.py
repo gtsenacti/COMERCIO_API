f780fd149a01640055c778b7ed2ccf50f9d440c2
gtsenacti <gt.senacti@gmail.com>
2025-06-18 21:36:48 -0300

    COMERCIO_API/Samuel-Conectando API ao banco de dados

--- API.py
+++ API.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
+#Tabela cadastro usuario
 @app.route("/CadastroUsuario", methods=["GET"])
 def listar_Cadastros():
     conn = conectar()
@@ -17,6 +17,23 @@ def listar_Cadastros():
     ]
     conn.close()
     return jsonify(dados)
+
+
+#Tabela cadastro produto 
+@app.route("/CadastroProduto", methods=["GET"])
+def listar_CadastrosProdutos():
+    conn = conectar()
+    #conn.execute("PRAGMA foreign_keys = ON") #ativa as chaves estrangeiras das tabelas (pois, não é ativado por padrão)
+    cursor = conn.cursor()
+    cursor.execute("SELECT IDCadastroProduto, Produto, Fornecedor, cnpj FROM CadastroProduto")
+    dados = [
+        {"id": row[0], "NomeProduto": row[1], "NomeFornecedor": row[2], "CNPJFornecedor": row[3]}
+        for row in cursor.fetchall()
+    ]
+    conn.close()
+    return jsonify(dados)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
